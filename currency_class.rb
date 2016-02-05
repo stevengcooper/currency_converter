537b109469e1fa2f320ce92292981f4a2bfaf219
Steven Cooper <stevengcooper76@gmail.com>
2016-02-05 06:51:13 -0500

    Multiply method is working.

--- currency_class.rb
+++ currency_class.rb
@@ -1,3 +1,6 @@
+# require './ DifferentCurrencyCodeError'
+# Currency < DifferentCurrencyCodeError
+
 class Currency
   attr_accessor :amount, :code
 
@@ -5,27 +8,39 @@ class Currency
     @code = code
     @amount = amount
   end
-
-  def error(other)
-    puts "DifferentCurrencyCodeError" if @code != other.code
+  # def error(other)
+  #   if @code != other.code
+  #     error_fixed = false
+  #     begin
+  #       raise "DifferentCurrencyCodeError" unless  error_fixed
+  #     rescue
+  #       error_fixed = true
+  #       retry
+  #     end
+  #   end
+  # end
+  def currency_error(value)
+    raise DifferentCurrencyCodeError, 'Currencies do not match.' if (@code == other.code) == false
   end
 
   def +(other)
-    @amount + other.amount if other.error
+    @amount + other.amount if other.currency_error
   end
 
   def -(other)
-    @amount - other.amount if other.error
+    @amount - other.amount if other.currency_error
   end
-  #
-  # def *(other)
-  #   @amount * other.amount if other_code.error
-  # end
+
+  def *(other)
+    @amount * other.amount if other.currency_error
+  end
 end
 
-a = Currency.new(:USSR, 500)
+a = Currency.new(:NAD, 500)
 c = Currency.new(:USD, 600.08)
+d = Currency.new(:USD, 10000)
 puts c.amount
 puts c.code
-puts c.amount + a.amount
-puts a.amount - c.amount
+puts c.amount + d.amount
+puts c.amount - d.amount
+puts c.amount * d.amount
